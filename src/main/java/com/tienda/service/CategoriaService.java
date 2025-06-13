31d559a8ffc4205e672552f75fd8d915e97c6d6a
Mariano2423 <marianocorrales2423@gmail.com>
2025-06-13 10:51:17 -0600

    Semana 5: CRUD

--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -13,21 +13,38 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CategoriaService {
-    
+
     //Se crea un objeto de manera unica (solo una instancia)para todo el proyecto y de manera automatica
     @Autowired
     private CategoriaRepository categoriaRepository;
-    
-    @Transactional(readOnly=true)
-    public List<Categoria> getCategorias(boolean activo){
+
+    @Transactional(readOnly = true)
+    public List<Categoria> getCategorias(boolean activo) {
         var lista = categoriaRepository.findAll();
-        
+
         // Se valida si solo se desean las categorias activas...
-        if(activo){
+        if (activo) {
             //Solo se quieren activas...
             lista.removeIf(c -> !c.isActivo());
         }
-        
+
         return lista;
+    }
+
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+
+        return categoriaRepository.findById(categoria.getIdCategoria())
+                .orElse(null);
+    }
+
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaRepository.save(categoria);
+    }
+
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaRepository.delete(categoria);
     }
 }
